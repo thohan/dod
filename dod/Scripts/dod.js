89bc12b4c7014b61bbe5437b03991331a34076df
thohan <bbean11@outlook.com>
2013-10-24 13:39:40 -0700

    Making stuff generic.

--- dod/Scripts/dod.js
+++ dod/Scripts/dod.js
@@ -1,24 +1,24 @@
 ﻿// create a model, etc. This'll be sort of the master file or something. I'm not sure how the js files will be organized.
 (function () {
-    namespace("leaf").model = function () {
-        var self = this;
+	namespace("leaf").model = function () {
+		var self = this;
 
-        self.preloader = html5preloader();
-        // note: what is this first screen called, before the title screen?
-        self.initialize = function () {
-            preloader.addFiles("myFile*:../Content/Images/tempTitle.png");
-            preloader.on("finish", function () {
-                var image = preloader.getFile("myFile");
-                var $canvas = $('#myCanvas');
-                $canvas.after(image);   // THis is nifty, but how can I know for sure that the image was actually preloaded? I guess I'll just have to trust it.
-            });
-        };
+		self.preloader = html5preloader();
+		// note: what is this first screen called, before the title screen?
+		self.initialize = function () {
+			preloader.addFiles("myFile*:../Content/Images/tempTitle.png");
+			preloader.on("finish", function () {
+				var image = preloader.getFile("myFile");
+				var $canvas = $('#myCanvas');
+				$canvas.after(image);	// THis is nifty, but how can I know for sure that the image was actually preloaded? I guess I'll just have to trust it.
+			});
+		};
 
-        return self;
-    };
+		return self;
+	};
 
-    $("document").ready(function () {
-        var model = namespace("leaf.model");
-        model().initialize();
-    });
+	$("document").ready(function () {
+		var model = namespace("leaf.model");
+		model().initialize();
+	});
 })();
