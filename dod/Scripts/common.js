85e424fc6ced8ecbab449623b31367e2fc41c5ad
thohan <bbean11@outlook.com>
2013-10-24 17:38:33 -0700

    Oops, missed these ones.

---
+++ dod/Scripts/common.js
@@ -0,0 +1,13 @@
+﻿function namespace(namespaceString) {
+    /// Generic namespace function that allows developers to create namespaces
+    /// <param name="namespaceString">namespace to create, i.e: "Product.Feature.Detail"</param>
+    /// <returns>namespace</returns>
+    var parts = namespaceString.split(".");
+    var parent = window;
+
+    $.each(parts, function (idx, part) {
+        parent[part] = parent[part] || {};
+        parent = parent[part];
+    });
+    return parent;
+}
