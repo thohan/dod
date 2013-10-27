1fc9f8afa88b4ca5e34dca277b713b7a1b5c517b
thohan <bbean11@gmail.com>
2013-10-26 22:00:41 -0600

    Getting a title screen and some buttons that start the game

--- dod/Scripts/dod.js
+++ dod/Scripts/dod.js
@@ -3,22 +3,57 @@
 	namespace("leaf").model = function () {
 		var self = this;
 
+		self.canvas = document.getElementById('myCanvas');
+
 		self.preloader = html5preloader();
 		// note: what is this first screen called, before the title screen?
 		self.initialize = function () {
-			preloader.addFiles("myFile*:../Content/Images/tempTitle.png");
-			preloader.on("finish", function () {
-				var image = preloader.getFile("myFile");
-				var $canvas = $('#myCanvas');
-				$canvas.after(image);	// THis is nifty, but how can I know for sure that the image was actually preloaded? I guess I'll just have to trust it.
+			// Preloading should ensure that various assets are available when it's time to draw them.
+			preloadAssets(self.preloader);
+
+			self.preloader.on("finish", function () {
+				// Now it's ok to start playing and whatnot.
+				drawTitleScreen();
 			});
+
 		};
+
+		function preloadAssets(preloader) {
+			preloader.addFiles("titleScreen*:../Content/Images/tempTitle.png");
+		}
+
+		function drawTitleScreen() {
+			if (self.canvas && self.canvas.getContext) {
+				var ctx = self.canvas.getContext('2d');
+				if (ctx) {
+					var img = self.preloader.getFile("titleScreen");
+					//img.src = self.preloader.getFile("titleScreen");
+					ctx.drawImage(img, 0.5, 0.5);
+
+					drawTitleButtons(ctx);
+				}
+			}
+		}
+
+		function drawTitleButtons(ctx) {
+			var offsetWidth = 50;
+			var offsetHeight = 15;
+			ctx.save();
+			ctx.translate(self.canvas.width / 2 - offsetWidth, self.canvas.height / 2 - offsetHeight);
+			ctx.moveTo(0, 0);
+			ctx.lineTo(100, 0);
+			ctx.lineTo(100, 30);
+			ctx.lineTo(0, 30);
+			ctx.lineTo(0, 0);
+			ctx.stroke();
+			ctx.restore();
+		}
 
 		return self;
 	};
 
 	$("document").ready(function () {
-		var model = namespace("leaf.model");
-		model().initialize();
+		var model = namespace("leaf").model();
+		model.initialize();
 	});
 })();
