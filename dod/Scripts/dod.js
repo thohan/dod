13fb6952b4a9cd8db504e45c771ce44877f65743
thohan <bbean11@gmail.com>
2013-11-16 23:15:10 -0700

    Created a second transition graphic thingy
    Lesson learned: layer.draw() is an expensive call when you're doing
    several hundred in a short time.

--- dod/Scripts/dod.js
+++ dod/Scripts/dod.js
@@ -9,7 +9,10 @@
 			height: 560
 		});
 
+		self.tileSize = 40;
+
 		self.preloader = html5preloader();
+
 		// note: what is this first screen called, before the title screen?
 		self.initialize = function () {
 			// Preloading should ensure that various assets are available when it's time to draw them.
@@ -18,8 +21,10 @@
 			self.preloader.on("finish", function () {
 				// Now it's ok to start playing and whatnot.
 				var bgLayer = new Kinetic.Layer();
+				self.stage.add(bgLayer);
 				drawImageToStage(0, 0, "titleScreen", 760, 560, bgLayer);
 				var buttonLayer = new Kinetic.Layer();
+				self.stage.add(buttonLayer);
 				var startButton = drawImageToStage(self.stage.getWidth() / 2 - 75, self.stage.getHeight() / 2 - 40, "buttonSprites", 150, 40, buttonLayer, 0, 0, 150, 40);
 				var continueButton = drawImageToStage(self.stage.getWidth() / 2 - 75, self.stage.getHeight() / 2 + 40, "buttonSprites", 150, 40, buttonLayer, 0, 40, 150, 40);
 				setStartButtonBehavior(startButton);
@@ -52,24 +57,21 @@
 					height: 40
 				});
 				button.draw();
-
 				var backRect = new Kinetic.Rect({
 					width: 760,
 					height: 560,
 					fill: "#383870",
 					visible: false
 				});
-
 				var layer = new Kinetic.Layer();
 				layer.add(backRect);
-				var crawlImage = drawImageToStage(0, 560, "startingCrawl", 760, 900, layer);
 				self.stage.add(layer);
 
 				drawTransitionOne(function () {
 					backRect.setVisible(true);
 					layer.draw();
 				}, function () {
-					drawStartCrawl(crawlImage, layer);
+					drawStartCrawl(layer, backRect);
 				});
 			});
 		}
@@ -92,13 +94,15 @@
 					height: 40
 				});
 				button.draw();
-				drawTransitionOne();
+				drawTransitionTwo(function () {
+					// Mid-transition code here
+				});
 				// TODO: Select from saved games
 			});
 		}
 
 		/// <summary>
-		/// Draws an image to the specified stage, in the specified location.
+		/// Draws an image to the specified layer, in the specified location.
 		/// Optionally, cropping coordinates may be added.
 		/// </summary>
 		function drawImageToStage(x, y, image, width, height, layer, cropX, cropY, cropWidth, cropHeight) {
@@ -122,15 +126,45 @@
 
 			// Consider some way to manage layers centrally, otherwise you're going to have layers running willy-nilly.
 			layer.add(imageObj);
-			self.stage.add(layer);
+			layer.draw();
+			return imageObj;
+		}
+
+		/// <summary>
+		/// Same as drawImageToStage() but the timeout will destroy the item!
+		/// </summary>
+		function drawImageToStageTimed(x, y, image, width, height, layer, cropX, cropY, cropWidth, cropHeight, timeout) {
+			var img = self.preloader.getFile(image);
+			var imageObj = new Kinetic.Image({
+				x: x,
+				y: y,
+				image: img,
+				width: width,
+				height: height
+			});
+
+			if (cropX !== undefined && cropY !== undefined && cropWidth !== undefined && cropHeight !== undefined) {
+				imageObj.setCrop({
+					x: cropX,
+					y: cropY,
+					width: cropWidth,
+					height: cropHeight
+				});
+			}
+
+			// Consider some way to manage layers centrally, otherwise you're going to have layers running willy-nilly.
+			layer.add(imageObj);
+			setTimeout(function () {
+				imageObj.destroy();
+			}, timeout);
 			return imageObj;
 		}
 
 		function drawTransitionOne(callback, callback2) {
 			var strokeWidth = 20;
 			var currentStroke = 0;
-			var canvasWidthCenter = self.stage.getWidth()/2;
-			var canvasHeightCenter = self.stage.getHeight()/2;
+			var canvasWidthCenter = self.stage.getWidth() / 2;
+			var canvasHeightCenter = self.stage.getHeight() / 2;
 			var counter = 0;
 			var goUp = true;
 			var rectArray = [];
@@ -141,10 +175,10 @@
 				if (goUp === true) {
 					var color = counter % 2 === 1 ? 'black' : 'green';
 					var rectangle = new Kinetic.Rect({
-						x: canvasWidthCenter - (currentStroke + 60 + strokeWidth/4),
-						y: canvasHeightCenter - (currentStroke + strokeWidth/4),
-						width: currentStroke * 2 + 120 + strokeWidth/4,
-						height: currentStroke * 2 + strokeWidth/4,
+						x: canvasWidthCenter - (currentStroke + 60 + strokeWidth / 4),
+						y: canvasHeightCenter - (currentStroke + strokeWidth / 4),
+						width: currentStroke * 2 + 120 + strokeWidth / 4,
+						height: currentStroke * 2 + strokeWidth / 4,
 						stroke: color,
 						strokeWidth: strokeWidth
 					});
@@ -155,7 +189,7 @@
 
 					currentStroke += strokeWidth;
 					counter++;
-					
+
 					if (counter > 20) {
 						goUp = false;
 						callback();	// This is where you can do whatever you need to under the fully-expanded transition animation.
@@ -170,22 +204,90 @@
 
 					if (counter === 0) {
 						clearInterval(interval);
-						callback2();
+						callback2();	// This is where you do whatever at the end of the transition
 					}
 				}
 			}, 75);
 		}
 
-		function drawStartCrawl(crawlImage, layer) {
+		function drawTransitionTwo(callback, callback2) {
+			var tsize = self.tileSize;
+			var layer = new Kinetic.Layer();
+			self.stage.add(layer);
+			var counter = 0;
+			var goUp = true;
+			var i;
+			var j;
+			var totalShapes = 0;
+			
+			var interval = setInterval(function () {
+				if (goUp === true) {
+					j = 0;
+					i = counter;
+					
+					while (i >= 0) {
+						if (i >= 0 && i < 19 && j >= 0 && j < 14) {
+							if (i >= 0) {
+								if ((i % 2 === 1 && j % 2 === 1) || (i % 2 === 0 && j % 2 === 0)) {
+									drawImageToStageTimed(i * tsize, j * tsize, "tileSprites", tsize, tsize, layer, 0, 0, tsize, tsize, 2000);
+								} else {
+									drawImageToStageTimed(i * tsize, j * tsize, "tileSprites", tsize, tsize, layer, tsize * 1, 0, tsize, tsize, 2000);
+								}
+								
+								totalShapes++;
+							}
+						}
+						
+						j++;
+						i--;
+					}
+
+					layer.draw();
+					counter++;
+
+					if (counter > 32) {
+						goUp = false;
+						callback();
+					}
+				} else {
+					layer.draw();
+					counter--;
+
+					if (counter <= 0) {
+						clearInterval(interval);
+					}
+				}
+			}, 60);
+		}
+
+		function drawStartCrawl(layer, backRect) {
+			var crawlImage = drawImageToStage(0, 560, "startingCrawl", 760, 900, layer);
+			var continueButton = drawImageToStage(620, 760, "buttonSprites", 150, 40, layer, 0, 40, 150, 40);
+			continueButton.on("mouseup", function () {
+				drawTransitionOne(function () {
+					backRect.destroy();
+					continueButton.destroy();
+					crawlImage.destroy();
+					// TODO: Add the game board!
+					layer.draw();
+				}, function () {
+					// what?
+				});
+			});
 			var interval = setInterval(function () {
 				crawlImage.setY(crawlImage.getY() - 1);
+
 				layer.draw();
-				
+
+				if (continueButton.getY() > 480) {
+					continueButton.setY(continueButton.getY() - 1);
+				}
+
 				if (crawlImage.getY() < -560) {
 					clearInterval(interval);
 				}
 			}, 20);
-			
+
 			// TODO: Add skip/continue buttons and their behaviors
 		}
 
